7305ae968988e393f43f9b0cf1e659d2fd1f6340
Vitor Vieira <vieirachp@hotmail.com>
2021-10-04 20:39:15 -0300

    feat: adds chi router

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+const addr = ":9090"
+
+func main() {
+	router := chi.NewRouter()
+	router.Route("/transaction/v1", func(r chi.Router) {
+		r.Post("/send", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("welcome"))
+		})
+	})
+	fmt.Printf("Server started at port %s", addr)
+
+	if err := http.ListenAndServe(addr, router); err != nil {
+		log.Fatal("could not initialize the server")
+	}
+}
