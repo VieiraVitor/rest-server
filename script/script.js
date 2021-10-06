4e52f7b4c1c0eb2ea0a0251b0399f81045ea2fd5
Vitor Vieira <vieirachp@hotmail.com>
2021-10-05 22:44:23 -0300

    test: adds script to run k8 benchmark

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+export let options = {
+	vus: 5000,
+	duration: '10s',
+}
+
+const url = 'http://127.0.0.1:8080/transaction/v1/send';
+const headers = {
+	headers: { 'Content-Type': 'application/json' }
+}
+const data = {
+	"id": 1,
+	"amount": 1000,
+	"description": "Novo produto",
+	"creditCard": {
+		"number": "1234",
+		"name": "Vitor",
+		"cvv": 123
+	}
+}
 
 export default function () {
-  http.post('http://localhost:8080/transaction/v1/send');
-  sleep(1);
+	http.post(url, JSON.stringify(data), { headers });
+	sleep(1);
 }
