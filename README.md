5e3641c6cc2113bb19d6cb66b2804e46a650c3bc
Vitor Vieira <vieirachp@hotmail.com>
2021-10-09 23:16:31 -0300

    test: update script and add data to run benchmarks

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 lib go-chi:
 https://github.com/go-chi/chi
+
+Hey benchmark test:
+hey -n 10000 -c 1000 -m POST -d '{"id": 1, "amount": 1000, "description": "Novo produto", "creditCard": { "number": "1234", "name": "Vitor", "cvv": 123 }}' http://localhost:8080/transaction/v1/send
--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { check } from 'k6';
 export let options = {
-	vus: 5000,
-	duration: '10s',
+	vus: 10000,
+	iterations: 100000,
 }
 
 const url = 'http://127.0.0.1:8080/transaction/v1/send';
@@ -20,7 +21,13 @@ const data = {
 	}
 }
 
+
 export default function () {
-	http.post(url, JSON.stringify(data), { headers });
+	const res = http.post(url, JSON.stringify(data), { headers });
+
+	check(res, {
+		'is status 200': (r) => r.status === 200,
+	});
+
 	sleep(1);
 }
